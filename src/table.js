66ec681dd883635ca14135f4ee764d9745a9fa9d
uchuukaeru <hirata.motoki@gmail.com>
2022-05-24 07:45:06 +0900

    Update table.js

--- src/table.js
+++ src/table.js
@@ -1,16 +1,21 @@
 export async function write_table(dataObj){
-    const keys=["MHP","MMP","ATK","DEF","AGL","MGC","SPR","TOTAL"];
+    const keys=["MHP","MMP","ATK","DEF","AGL","MGC","SPR"];
     const labels=["num","rank"];
     console.log(dataObj);
-    for(let i=0;i<keys.length;i++){
+    for(let i=0;i<7;i++){
         for(let n=0;n<labels.length;n++){
             let id=keys[i]+"_"+labels[n];
-            console.log(id);
             let data=String(dataObj["status"][labels[n]][keys[i]]);
-            console.log(data);
+            console.log(id,":",data);
             let span=document.getElementById(id);// html内の変数idの要素にアクセスする
             span.innerHTML=data;
         }
     }
-
+    for(let i=0;i<labels.length;i++){
+        let id="TOTAL_"+labels[n];
+        let data=String(dataObj["TOTAL"][labels[i]]);
+        console.log(id,":",data);
+        let span=document.getElementById(id);// html内の変数idの要素にアクセスする
+        span.innerHTML=data;
+    }
 }
