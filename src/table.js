93566515a7f95b15d5d9391685c9ea7a535190bf
uchuukaeru <hirata.motoki@gmail.com>
2022-05-24 07:04:44 +0900

    Update table.js

--- src/table.js
+++ src/table.js
@@ -5,7 +5,9 @@ export function write_table(dataObj){
     for(let i=0;i<7;i++){
         for(let n=0;n<2;n++){
             let id=keys[i]+"_"+labels[n];
-            let data=dataObj["status"][labels[n]][keys[i]];
+            console.log(id);
+            let data=String(dataObj["status"][String(labels[n])][String(keys[i])]);
+            console.log(data);
             let span=document.getElementById(id);// html内の変数idの要素にアクセスする
             span.innerHTML(data);
         }
