45bf3e208bc48aeaf20526a27273b3b14b9d5d66
uchuukaeru <hirata.motoki@gmail.com>
2022-05-24 07:42:00 +0900

    Update table.js

--- src/table.js
+++ src/table.js
@@ -2,8 +2,8 @@ export async function write_table(dataObj){
     const keys=["MHP","MMP","ATK","DEF","AGL","MGC","SPR","TOTAL"];
     const labels=["num","rank"];
     console.log(dataObj);
-    for(let i=0;i<7;i++){
-        for(let n=0;n<2;n++){
+    for(let i=0;i<keys.length;i++){
+        for(let n=0;n<labels.length;n++){
             let id=keys[i]+"_"+labels[n];
             console.log(id);
             let data=String(dataObj["status"][labels[n]][keys[i]]);
