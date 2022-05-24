9c06475f771f93d936e9f8cda8f901ddbf35b00b
uchuukaeru <hirata.motoki@gmail.com>
2022-05-24 23:51:42 +0900

    Update used_json.js

--- src/used_json.js
+++ src/used_json.js
@@ -28,8 +28,7 @@ function data(dpath,id){// data.htmlのデータを出力する
         if(request.readyState==4 && request.status==200){
             let json=JSON.parse(request.responseText);
             console.log(json);
-            console.log(json[String(id)]);
-            console.log(json[id]);
+            console.log(json.id);
             json=json[String(id)];
             console.log(json);
             document.title=id+":"+json["名前"];//titleを変更する
