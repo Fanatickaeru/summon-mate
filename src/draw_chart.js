3c4fe9e0ea01db81fa91af2ae2b8fb2c24c3c043
uchuukaeru <hirata.motoki@gmail.com>
2022-05-24 11:54:10 +0900

    add file
    横向きグラフの表示用の調整

--- src/draw_chart.js
+++ src/draw_chart.js
@@ -14,32 +14,32 @@ export function draw(id,type,dataObj){// グラフを描画する・moduleとし
                 MStatus=Object.values(dataObj["status"]["num"]);
                 MAvg=Object.values(dataObj["avg"]);
             }
-            else if(type=="bar"){
+            else if(type=="horizontalBar"){
                 MStatus.push(dataObj["TOTAL"]["num"]);
                 MAvg.push(dataObj["TOTAL"]["avg"]);
             }
             // console.log(MStatus);
             // console.log(MAvg);
 
-            // setlist["data"]["datasets"][0]["data"]=MStatus;
-            // setlist["data"]["datasets"][1]["data"]=MAvg;
+            setlist["data"]["datasets"][0]["data"]=MStatus;
+            setlist["data"]["datasets"][1]["data"]=MAvg;
 
-            // let stalist=MStatus.concat(MAvg);
-            // let max=0;// データの最大値を保存する変数
+            let stalist=MStatus.concat(MAvg);
+            let max=0;// データの最大値を保存する変数
 
             // console.log(stalist);
 
-            // stalist.forEach(function(element){// stalistの最大値を求める
-            //     if(max<element) max=element;
-            // });
+            stalist.forEach(function(element){// stalistの最大値を求める
+                if(max<element) max=element;
+            });
 
             setlist["data"]["datasets"][0]["label"]=document.getElementById("name").innerHTML;// グラフのラベルを<id="name">にする
             if(type=="radar"){
-                // setlist["options"]["scale"]["ticks"]["max"]=parseInt(max+50);// グラフの最大値をmax+50の範囲にする
+                setlist["options"]["scale"]["ticks"]["max"]=parseInt(max+50);// グラフの最大値をmax+50の範囲にする
             }
-            else if(type=="bar"){            
+            else if(type=="horizontalBar"){            
                 // console.log("data:",setlist["options"]["scales"]["xAxes"][0]["ticks"]);
-                // setlist["options"]["scales"]["xAxes"][0]["ticks"]["max"]=parseInt(max+50);
+                setlist["options"]["scales"]["xAxes"][0]["ticks"]["max"]=parseInt(max+50);
                 console.log(setlist)
             }
             let chart = new Chart(ctx,setlist);// グラフを描画する
