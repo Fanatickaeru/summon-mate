442864ac2b9c4de3b8dd8bf1557a2db866d03398
uchuukaeru <hirata.motoki@gmail.com>
2022-05-24 11:19:05 +0900

    Update draw_chart.js

--- src/draw_chart.js
+++ src/draw_chart.js
@@ -37,6 +37,7 @@ export function draw(id,type,dataObj){// グラフを描画する・moduleとし
             else if(type=="bar"){            
                 console.log("data:",setlist["options"]["scales"]["xAxes"][0]["ticks"]);
                 setlist["options"]["scales"]["xAxes"][0]["ticks"]["max"]=parseInt(max+50);
+                console.log(setlist)
             }
             let chart = new Chart(ctx,setlist);// グラフを描画する
         }
